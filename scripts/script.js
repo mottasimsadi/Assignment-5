1c13e7f4374415eeecde772d6a2b3501a8964382
mottasimsadi <mottasimsadi@gmail.com>
2025-03-02 23:35:46 +0600

    Implemented the functionality of the completed task button with alert in the homepage for the challenge part

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
-// Challenge Part Starts Here
+// Challenge Part (Random Background Colour) Starts Here
 
-// Random Background Colour
 function randomColour() {
   let colours = [
     "#F28D35",
@@ -32,21 +31,18 @@ document
       .classList.add("bg-[" + randomColour() + "]");
   });
 
-// Challenge Part Ends Here
+// Challenge Part (Random Background Colour) Ends Here
 
-// Challenge Part Starts Here
-
-// Jumping into Blog Page
+// Challenge Part (Jumping into Blog Page) Starts Here
 
 document.getElementById("goto-blogs").addEventListener("click", function () {
   window.location.href = "blog.html";
 });
 
-// Challenge Part Ends Here
+// Challenge Part (Jumping into Blog Page) Ends Here
 
-// Challenge Part Starts Here
+// Challenge Part (Get Current Date) Starts Here
 
-// Get Current Date
 const weekDays = ["Sun,", "Mon,", "Tue,", "Wed,", "Thu,", "Fri,", "Sat,"];
 const fullDate = new Date();
 let day = fullDate.getDay();
@@ -75,4 +71,44 @@ document.getElementById("date").innerHTML = date;
 let year = fullDate.getFullYear();
 document.getElementById("year").innerHTML = year;
 
-// Challenge Part Ends Here
+// Challenge Part (Get Current Date) Ends Here
+
+// Challenge Part (Completed Task Button with Alert) Ends Here
+
+const allCompletedButtons = document.querySelectorAll(".btn-completed");
+let countClick = 0;
+for (let allCompletedButton of allCompletedButtons) {
+  allCompletedButton.addEventListener("click", function (event) {
+    alert("Board Updated Successfully");
+
+    // Decrease the Currently Assigned Task Value
+    const assignedTask = getInnerTextById("task-assigned-currently");
+    document.getElementById("task-assigned-currently").innerText = Math.max(0, assignedTask - 1);
+
+    // Add and Update the Total Completed Task Value
+    const totalCompletedTask = getInnerTextById("total-task-completed");
+    document.getElementById("total-task-completed").innerText = totalCompletedTask + 1;
+
+    // Disable The Completed Button after the Task is Completed
+    event.target.disabled = true;
+
+    // Add the Activity Log History with Real Time
+    const realTime = new Date();
+    let clickTime = realTime.toLocaleTimeString();
+    const history = document.getElementById("history");
+    const p = document.createElement("p");
+    const taskTitle = allCompletedButton.parentElement.parentElement.parentElement.childNodes[3].innerText;
+    p.innerText = `You have Completed The Task ${taskTitle} at ${clickTime}`;
+    p.className = "mb-7 p-3 bg-[#F4F7FF] rounded-lg";
+    history.appendChild(p);
+
+    // Add Extra Alert
+    const allTaskCompleted = allCompletedButtons.length;
+    countClick = countClick + 1;
+    if (countClick === allTaskCompleted) {
+      alert("Congrates!!! You have completed all current task");
+    }
+  });
+}
+
+// Challenge Part (Completed Task Button with Alert) Starts Here
